ac4e30c60421ddd9fe628d207f5595e518111642
Rohan Daivajna <142878012+RohanDaivajna@users.noreply.github.com>
2025-05-19 02:53:47 +0530

    Update route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -34,7 +34,7 @@ export async function GET(req: NextRequest) {
  return NextResponse.redirect(
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000/"
-      : "https://reddit-yt-ten.vercel.app/"
+      : "https://stack-whirl.vercel.app//"
   );
 
 }

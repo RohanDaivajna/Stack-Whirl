b2e9a490a5dd4da4299d37878e20ca850fc971b1
Rohan Daivajna <rohan11daivajna@gmail.com>
2025-05-19 02:34:40 +0530

    finished project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import StackWhirl from "../../public/logo-name.png";
+import logo from "../../public/logo.png";
+import Image from "next/image";
+import { ThemeToggle } from "./ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { UserDropdown } from "./UserDropdown";
+
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between">
+      <Link href="/" className="flex items-center gap-x-3">
+        <Image
+          src={logo}
+          alt="StackWhirl mobile icon"
+          className="h-10 w-fit"
+        />
+        <Image
+          src={StackWhirl}
+          alt="StackWhirl Desktop"
+          className="h-15 w-fit hidden lg:block"
+        />
+      </Link>
+
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        {user ? (
+          <UserDropdown userImage={user.picture} />
+        ) : (
+          <div className="flex items-center gap-x-4">
+            <Button variant="secondary" asChild>
+              <RegisterLink>Sign up</RegisterLink>
+            </Button>
+            <Button asChild>
+              <LoginLink>Log in</LoginLink>
+            </Button>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatar.vercel.sh",
+        port: "",
+      },
+      {
+        hostname: "utfs.io",
+        port: "",
+      },
+      {
+        
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
